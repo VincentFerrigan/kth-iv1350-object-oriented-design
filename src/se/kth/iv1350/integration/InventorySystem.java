ce4fafc8e0f6874d155bdb654912b339f8ecc25f
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-25 15:27:20 +0200

    No need to create items separately

--- src/se/kth/iv1350/integration/InventorySystem.java
+++ src/se/kth/iv1350/integration/InventorySystem.java
@@ -9,27 +9,24 @@ import java.util.Map;
 
 public class InventorySystem {
     private Map<Integer, Item> inventoryTable = new HashMap<>();
-    private Map<String, String> testTable = new HashMap<>(); // TODO enbart test. Ta bort efter
 
 
-//    public InventorySystem(File csvFile){}
-
+    //    public InventorySystem(File csvFile){}
     public InventorySystem(){
         // TODO skall läsa in från csv fil som skickas med som argument.
-        //  Nedan är en hårdkodning tillsvidare
-        ItemDTO marlboro = new ItemDTO(2001, "Marlboro", " American brand of cigarettes,", 70, 1);
+        // TODO Nedan är en hårdkodning tillsvidare
+        ItemDTO marlboro = new ItemDTO(2001, "Marlboro", "Marlboro Cigaretter. 20 st/pkt", 70, 1);
         ItemDTO festis = new ItemDTO(1001, "Festis Apelsin", "Festis. 50cl.", 22, 1);
-//    Item m = new Item(marlboro, 10);
-//    Item f = new Item(festis, 10);
 
         inventoryTable.put(marlboro.getItemID(), new Item(marlboro, 10));
         inventoryTable.put(festis.getItemID(), new Item(festis, 4));
     }
 
-    public ItemDTO registerItem(int itemID){
+    public ItemDTO getItemInfo(int itemID){
+//    public ItemDTO registerItem(int itemID){
         Item item = this.inventoryTable.get(itemID);
         item.decrement();
-        this.inventoryTable.put(itemID, item); // Någåot osäker om replace är korrekt
+        this.inventoryTable.put(itemID, item); // Något osäker om replace eller ens put behövs
         return item.getItemDTO();
     }
 
