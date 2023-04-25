a2493ce75e9ce28ac84c07f58f69e41213cce272
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-25 13:45:19 +0200

    Switched to Map instead of table

--- src/se/kth/iv1350/integration/InventorySystem.java
+++ src/se/kth/iv1350/integration/InventorySystem.java
@@ -4,39 +4,37 @@ import src.se.kth.iv1350.dto.ItemDTO;
 import src.se.kth.iv1350.dto.SaleDTO;
 import src.se.kth.iv1350.model.Item;
 
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InventorySystem {
-    Hashtable<String, Item> inventoryTable = new Hashtable<String, Item>();
-    Hashtable<String, String> testTable = new Hashtable<String, String>(); // TODO enbart test. Ta bort efter
+    private Map<Integer, Item> inventoryTable = new HashMap<>();
+    private Map<String, String> testTable = new HashMap<>(); // TODO enbart test. Ta bort efter
 
-    // TODO skall läsa in från csv fil. Nedan är en hårdkodning tillsvidare
-    ItemDTO marlboro = new ItemDTO(2001, "Marlboro", " American brand of cigarettes,", 70, 1);
-    ItemDTO festis = new ItemDTO(1001, "Festis Apelsin", "Festis. 50cl.", 22, 1);
-    Item m = new Item(marlboro, 10);
-    Item f = new Item(festis, 10);
 
-//    inventoryTable.put(Integer.toString(cigg.getItemID()), new Item(marlboro, 10));
-//    inventoryTable.put(Integer.toString(festis.getItemID()), new Item(festis, 4));
-    inventoryTable.put(1001, m);
-    testTable.put("testkey", "testval");
+//    public InventorySystem(File csvFile){}
 
     public InventorySystem(){
-        //TODO  Constructor
-        // set up hashtable here? From txt file
+        // TODO skall läsa in från csv fil som skickas med som argument.
+        //  Nedan är en hårdkodning tillsvidare
+        ItemDTO marlboro = new ItemDTO(2001, "Marlboro", " American brand of cigarettes,", 70, 1);
+        ItemDTO festis = new ItemDTO(1001, "Festis Apelsin", "Festis. 50cl.", 22, 1);
+//    Item m = new Item(marlboro, 10);
+//    Item f = new Item(festis, 10);
+
+        inventoryTable.put(marlboro.getItemID(), new Item(marlboro, 10));
+        inventoryTable.put(festis.getItemID(), new Item(festis, 4));
     }
 
     public ItemDTO registerItem(int itemID){
-        Item item = this.inventoryTable.get(Integer.toString(itemID));
+        Item item = this.inventoryTable.get(itemID);
         item.decrement();
-        this.inventoryTable.replace(Integer.toString(itemID), item); // Någåot osäker om replace är korrekt
-        return item.getItemDTO;
-
-        return this.inventoryTable.get(Integer.toString(itemID)).getItem();
+        this.inventoryTable.put(itemID, item); // Någåot osäker om replace är korrekt
+        return item.getItemDTO();
     }
 
     public void updateInventory(SaleDTO saleInfo){
-        saleInfo
+//        saleInfo
         //TODO do it
     }
 }
