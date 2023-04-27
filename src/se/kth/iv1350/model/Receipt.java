87e9b429570cd211d3d93808c517dc6d280bd86e
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-27 21:21:39 +0200

    First draft of receipt.

--- src/se/kth/iv1350/model/Receipt.java
+++ src/se/kth/iv1350/model/Receipt.java
@@ -1,12 +1,45 @@
 package src.se.kth.iv1350.model;
 
-import src.se.kth.iv1350.dto.SaleDTO;
-import src.se.kth.iv1350.integration.Printer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
+// TODO package private kontruktor och final attribute?
 public class Receipt {
-    Receipt(SaleDTO saleInfo, Printer printer){
-        // TODO Construct
+    private final Sale sale;
+    Receipt(Sale sale){
+        this.sale = sale;
     }
+    @Override
+    public String toString() {
+        List<Item> listOfItems = new ArrayList<>(sale.getCollectionOfItems());
 
-    // Print the DTO. And the DTO has a toString()
+        // Momsberäkning
+        Amount totalVATAmount = new Amount(0);
+        List<Amount> vatAmounts = listOfItems.stream().map(Item::getVatAmount).collect(toList());
+        totalVATAmount = totalVATAmount.plus(vatAmounts);
+
+        // Sorterar listan per namn
+        Collections.sort(listOfItems, Comparator.comparing(Item::getName));
+
+        // Pretty printing
+        StringBuilder builder = new StringBuilder();
+        for (Item item: listOfItems) {
+            builder.append("Item: " + item.getItemDTO().getName() + ", "); //TODO är det namn eller description
+            builder.append(item.getTotalAmount());
+            builder.append("\n");
+        }
+        builder.append("Total VAT:\t" + totalVATAmount);
+        builder.append("\n");
+        builder.append("Total cost:\t" + this.sale.getPayment().getTotalCost());
+        builder.append("\n");
+        builder.append("Paid amount:\t " + this.sale.getPayment().getPaidAmt());
+        builder.append("\n");
+        builder.append("Change:\t" + this.sale.getPayment().getChange());
+        builder.append("\n");
+        return builder.toString();
+    }
 }
