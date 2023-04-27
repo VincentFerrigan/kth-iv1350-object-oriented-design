a64d6f0daee318a5f6c399a57ce4397b225d5662
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-27 21:40:31 +0200

    No longer returning objects to view. Goal is to follow MVC and increase cohesion. Also adding pay and receipt functionality.

--- src/se/kth/iv1350/model/Sale.java
+++ src/se/kth/iv1350/model/Sale.java
@@ -1,13 +1,10 @@
 package src.se.kth.iv1350.model;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.function.BiFunction;
 
-import src.se.kth.iv1350.dto.CurrentSaleDTO;
 import src.se.kth.iv1350.dto.DiscountDTO;
 import src.se.kth.iv1350.dto.ItemDTO;
 import src.se.kth.iv1350.dto.SaleDTO;
-import src.se.kth.iv1350.model.Receipt;
 import src.se.kth.iv1350.integration.Printer;
 
 import static java.util.stream.Collectors.toList;
@@ -16,7 +13,9 @@ public class Sale {
     private LocalDateTime timeOfSale;
     private Amount runningTotal;
     private Map<Integer, Item> items = new HashMap<>(); // TODO Ändra namn till shoppingCart?
-    private Receipt receipt;            //TODO where is this from?
+    private CashPayment payment;
+
+//    private InventorySystem is; // För att kunna plocka från "lagret". Men då måste 'is' skickas med från kontrollern när Sale instansieras.
 
     /**
      * Create a new instance and saves the time of sale.
@@ -26,11 +25,11 @@ public class Sale {
         this.runningTotal = new Amount(0);
     }
 
-    public CurrentSaleDTO addItem(ItemDTO itemInfo){
-        return addItem(itemInfo, 1);
+    public void addItem(ItemDTO itemInfo){
+        addItem(itemInfo, 1);
 
     }
-    public CurrentSaleDTO addItem(ItemDTO itemInfo, int quantity){
+    public void addItem(ItemDTO itemInfo, int quantity){
         Item additionalItem = new Item(itemInfo, quantity);
 
         int key = itemInfo.getItemID(); // TODO hämta nyckeln från itemInfo eller additionalItem?
@@ -39,10 +38,7 @@ public class Sale {
         } else {
             items.put(key, additionalItem);
         }
-//        this.runningTotal.addAmount(additionalItem.getTotalAmount());
         this.runningTotal = this.runningTotal.plus(additionalItem.getTotalAmount());
-        Item[] itemArray = getItemArraySortedByTimeOfUpdate();
-        return new CurrentSaleDTO(itemArray, this.runningTotal);
     }
 
     private void increaseQuantity(){
@@ -53,14 +49,9 @@ public class Sale {
         //TODO what is happening here? is that the attribute? SaleDTO?
     }
 
-    private void calclationOfPrice(){
-//        items.forEach();
-        //TODO needs an attribute OR is this where we use SaleDTO?
+    CashPayment getPayment(){
+        return payment;
     }
-
-    // Alt 4. TODO sorterad? Alfabetiskt? När den las till? I sådana fall behöver vi göra om det hela till en list.
-    // Alt 3. Göra en orentlig kopia för att undvika en shallow copy?
-
     public Amount getRunningTotal() {
         return runningTotal;
     }
@@ -70,33 +61,27 @@ public class Sale {
         return itemCollection.toArray(new Item[0]);
     }
 
-    private Item[] getItemArraySortedByItemID() {
-        List<Item> listOfItems = new ArrayList<>(items.values());
-        Collections.sort(listOfItems, Comparator.comparing(Item::getItemID));
-        return listOfItems.toArray(new Item[0]);
-    }
-    private Item[] getItemArraySortedByTimeOfUpdate() {
-        List<Item> listOfItems = new ArrayList<>(items.values());
-//        Collections.sort(listOfItems, Comparator.comparing(Item::getTimeOfUpdate));
-//        Collections.reverse(listOfItems);
-        Collections.sort(listOfItems, Comparator.comparing(Item::getTimeOfUpdate).reversed());
-        return listOfItems.toArray(new Item[0]);
+    Collection<Item> getCollectionOfItems() {
+        return items.values();
     }
 
+    // TODO. Bör tas bort/flyttas för att få High Cohesion.
     private Item[] getItemArraySortedByItemName() {
         List<Item> listOfItems = new ArrayList<>(items.values());
         Collections.sort(listOfItems, Comparator.comparing(Item::getName));
         return listOfItems.toArray(new Item[0]);
     }
 
+    // TODO Bör nog ändras. Samma upplägg som Display. Logging kan ske med hjälp av SaleLog.
     public SaleDTO endSale(){
         Amount totalVATAmount = new Amount(0);
         Item[] itemArray = getItemArraySortedByItemName();
         List<Amount> vatAmounts = items.values().stream().map(Item::getVatAmount).collect(toList());
         totalVATAmount = totalVATAmount.plus(vatAmounts);
-        Amount amountPaid = new Amount(0);
-        Amount changeAmount = new Amount(0);
-        return new SaleDTO(runningTotal, itemArray, timeOfSale, totalVATAmount, amountPaid, changeAmount);
+//        Amount amountPaid = new Amount(0);
+//        Amount changeAmount = new Amount(0);
+//        return new SaleDTO(runningTotal, itemArray, timeOfSale, totalVATAmount, amountPaid, changeAmount);
+        return new SaleDTO(runningTotal, itemArray, timeOfSale, totalVATAmount);
     }
 
     public SaleDTO applyDiscount(DiscountDTO discount){
@@ -104,21 +89,34 @@ public class Sale {
         return endSale();
     }
 
-    public SaleDTO pay(CashPayment payment){
-//        Amount vatAmount = new Amount(0);
-//        Item[] itemArray = getItemArraySortedByItemName(); // TODO hur vill du ha den sorterad?
-//        for (Item item: itemArray) {
-//            vatAmount.addAmount(item.getVatAmount());
-//        }
-        Amount totalVATAmount = new Amount(0);
-        Item[] itemArray = getItemArraySortedByItemName();
-        List<Amount> vatAmounts = items.values().stream().map(Item::getVatAmount).collect(toList());
-        totalVATAmount = totalVATAmount.plus(vatAmounts);
-        CashPayment amountPaid = payment;
-        amountPaid.calculateTotalCost(this);
-//        amountPaid.setTotalCost(runningTotal);
-        return new SaleDTO(runningTotal, itemArray, timeOfSale, totalVATAmount, amountPaid.getPaidAmt(), amountPaid.getChange());
-        // Tror då att CashPayment behöver kunna subtrahera.
-    }
-}
 
+    public void pay(CashPayment payment) {
+        payment.calculateTotalCost(this);
+        this.payment = payment;
+    }
+    public void printReceipt(Printer printer) {
+        Receipt receipt = new Receipt(this);
+        printer.print(receipt);
+    }
+
+    public void displayCurrentSale(Printer printer) {
+        Display display = new Display(this);
+        printer.print(display);
+    }
+//    public SaleDTO pay2(CashPayment payment){
+////        Amount vatAmount = new Amount(0);
+////        Item[] itemArray = getItemArraySortedByItemName(); // TODO hur vill du ha den sorterad?
+////        for (Item item: itemArray) {
+////            vatAmount.addAmount(item.getVatAmount());
+////        }
+//        Amount totalVATAmount = new Amount(0);
+//        Item[] itemArray = getItemArraySortedByItemName();
+//        List<Amount> vatAmounts = items.values().stream().map(Item::getVatAmount).collect(toList());
+//        totalVATAmount = totalVATAmount.plus(vatAmounts);
+//        CashPayment amountPaid = payment;
+//        amountPaid.calculateTotalCost(this);
+////        amountPaid.setTotalCost(runningTotal);
+//        return new SaleDTO(runningTotal, itemArray, timeOfSale, totalVATAmount, amountPaid.getPaidAmt(), amountPaid.getChange());
+//        // Tror då att CashPayment behöver kunna subtrahera.
+//    }
+}
