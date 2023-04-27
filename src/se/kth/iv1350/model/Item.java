07a0e23cd2fd09d765331f86fdba706d016aee6e
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-27 13:12:54 +0200

    No need for itemID attribute. An indirect getter is used instead. Time attribute added in order to sort shopping cart CurrentSaleDTO per item added. An indirect getter is used for item name, in order to sort SaleDTO per name. Reordered/restructured code.

--- src/se/kth/iv1350/model/Item.java
+++ src/se/kth/iv1350/model/Item.java
@@ -1,16 +1,16 @@
 package src.se.kth.iv1350.model;
 import src.se.kth.iv1350.dto.ItemDTO;
 
+import java.time.LocalDateTime;
+
 public class Item {
-    // TODO kan man göra den generiskt? Så den kör samma typ som ItemDTO
-    private final int itemID;
     private final ItemDTO itemInfo;
+    private LocalDateTime timeOfUpdate;
     private int quantity;
     private Amount totalAmount;
 
-
     public Item (ItemDTO item, int quantity){
-        this.itemID = item.getItemID();
+        this.timeOfUpdate = LocalDateTime.now();
         this.itemInfo = item;
         this.totalAmount = new Amount(itemInfo.getPrice());
         this.quantity = quantity;
@@ -19,65 +19,48 @@ public class Item {
     public Item (ItemDTO item){
         this(item, 1);
     }
-
-    // För salelog/sales
-    /**
-     * Increment an already added item when it's more than one of the same.
-     */
-    public void increment(){
-        this.quantity++;
+    public void addItem(Item termItem){
+        this.timeOfUpdate = LocalDateTime.now();
+        if (this.equals(termItem)) {
+            addToQuantity(termItem.getQuantity());
+            totalAmount.addAmount(termItem.getTotalAmount());
+        }
     }
-
-    // För när vi uppdaterar inventory.
-    public void decrement(){
-        this.quantity--;
-    }
-
     /**
      * Sets the quantity of items with same ID number.
      * @param   quantity    nbr of items
      */
     public void setQuantity(int quantity){
+        this.timeOfUpdate = LocalDateTime.now();
         this.quantity = quantity;
     }
 
     public void addToQuantity(int additionalQuantity){
+        this.timeOfUpdate = LocalDateTime.now();
         this.quantity += additionalQuantity;
+    }
+
+    // Getters
+    public LocalDateTime getTimeOfUpdate() {
+        return timeOfUpdate;
     }
     public ItemDTO getItemDTO() {
         return itemInfo;
     }
 
     public int getItemID() {
-        return itemID;
+        return itemInfo.getItemID();
     }
 
     public int getQuantity() {
         return quantity;
     }
-    //TODO ny metod!
 
     /**
-     * Returns to total amount i.e. the item price x quantity.
+     * Returns the total amount i.e. the item price x quantity.
      */
     public Amount getTotalAmount() {
         return totalAmount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {return true;}
-
-        Item c = (Item) o;
-        return Integer.compare(itemID, c.getItemID()) == 0;
-    }
-
-    // exemption?
-    public void addItem(Item termItem){
-        if (this.equals(termItem)) {
-            addToQuantity(termItem.getQuantity());
-            totalAmount.addAmount(termItem.getTotalAmount());
-        }
     }
 
     public Amount getVatAmount() {
@@ -85,5 +68,33 @@ public class Item {
         double vatRate = itemInfo.getVATRate();
         vatAmount.multiptyAmount(vatRate);
         return vatAmount;
+    }
+
+    public String getName() {
+        return itemInfo.getName();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {return true;}
+        Item c = (Item) o;
+        return Integer.compare(this.getItemID(), c.getItemID()) == 0;
+    }
+
+    // TODO exemptions?
+    // TODO increment och decrement()? OBS! uppdatera totalbelopp!
+    // Eventuellt för salelog/sales
+    /**
+     * Increment an already added item when it's more than one of the same.
+     */
+    public void increment(){
+        this.timeOfUpdate = LocalDateTime.now();
+        this.quantity++;
+    }
+
+    // TODO increment och decrement()? OBS! uppdatera totalbelopp!
+    // Eventuellt för när vi uppdaterar inventory.
+    public void decrement(){
+        this.timeOfUpdate = LocalDateTime.now();
+        this.quantity--;
     }
 }
