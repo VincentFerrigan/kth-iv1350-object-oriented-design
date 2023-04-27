952bf1e813517669347bb35fa6d438787c02dd5c
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-27 21:21:02 +0200

    Adding print receipt and display functionality.

--- src/se/kth/iv1350/integration/Printer.java
+++ src/se/kth/iv1350/integration/Printer.java
@@ -1,5 +1,6 @@
 package src.se.kth.iv1350.integration;
 
+import src.se.kth.iv1350.model.Display;
 import src.se.kth.iv1350.model.Receipt;
 
 public class Printer {
@@ -7,7 +8,21 @@ public class Printer {
         //TODO construct
     }
 
-    public void printReceipt(Receipt currentReceipt){
-        //TODO it needs to do it
+    public void print(Receipt receipt) {
+        System.out.println("----------------- Receipt follows --------------");
+        System.out.println(receipt);
+        System.out.println("------------------ End of receipt --------------");
+
+    }
+    public void print(SaleLog saleLog) {
+        // TODO
+
+    }
+
+    public void print(Display display) {
+        System.out.println("----------------- Display follows --------------");
+        System.out.println(display);
+        System.out.println("------------------ End of Display --------------");
+
     }
 }
