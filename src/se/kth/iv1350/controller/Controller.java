8957a3625f08ee16571b551474f6c2c7a1f91362
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-28 11:41:45 +0200

    Preparing for discount system operation.

--- src/se/kth/iv1350/controller/Controller.java
+++ src/se/kth/iv1350/controller/Controller.java
@@ -1,6 +1,8 @@
 package src.se.kth.iv1350.controller;
 
+import src.se.kth.iv1350.dto.DiscountDTO;
 import src.se.kth.iv1350.dto.ItemDTO;
+import src.se.kth.iv1350.integration.DiscountRegister;
 import src.se.kth.iv1350.integration.InventorySystem;
 import src.se.kth.iv1350.integration.Printer;
 import src.se.kth.iv1350.model.CashPayment;
@@ -14,18 +16,18 @@ public class Controller {
     private SaleLog saleLog;
     private InventorySystem is = new InventorySystem("src/se/kth/iv1350/startup/inventory_items.csv");
     private CashRegister cashRegister;
+    private DiscountRegister discountRegister = new DiscountRegister();
     private Sale currentSale;
 
     /**
      * Constructor
-     * @param printer
-     * @param saleLog
+     * @param printer The printer.
+     * @param saleLog The object that loggs all transactions.
      */
     public Controller (Printer printer, SaleLog saleLog){
         this.printer = printer;
         this.saleLog = saleLog;
         this.cashRegister = new CashRegister();
-
     }
 
     /**
@@ -51,12 +53,16 @@ public class Controller {
 
     public void endSale(){
         currentSale.endSale();
+        currentSale.displayEndOfSale(printer);
     }
 
-//    public void discountRequest (int customerID){
+    public void discountRequest (int customerID){
         //TODO do it
-//        currentSale.endSale(customerID);
-//    }
+        DiscountDTO discountDTO = discountRegister.getDiscount(customerID);
+        currentSale.applyDiscount(discountDTO);
+        currentSale.endSale();
+        currentSale.displayEndOfSale(printer);
+    }
 
     public void pay(Amount paidAmt){
         CashPayment payment = new CashPayment(paidAmt);
@@ -65,7 +71,7 @@ public class Controller {
         cashRegister.addPayment(payment);
         saleLog.logSale(currentSale);
         // 1.5 UpdateInventory   // TODO till txt fil?
-        // 1.6 AccoutingSystem.updateToAccounting   // TODO till txt fil?
+        // 1.6 AccountingSystem.updateToAccounting   // TODO till txt fil?
         currentSale.printReceipt(printer);
         currentSale = null;
     }
