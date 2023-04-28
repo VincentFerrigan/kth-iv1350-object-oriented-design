c519f969e5da416510ae1da2b7b4efde5e6b13b5
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-28 15:51:38 +0200

    Restructuring display, we have enabled the option to return DTOs to view.

--- src/se/kth/iv1350/model/Display.java
+++ src/se/kth/iv1350/model/Display.java
@@ -1,5 +1,8 @@
 package src.se.kth.iv1350.model;
 
+import src.se.kth.iv1350.dto.SaleDTO;
+import src.se.kth.iv1350.dto.SaleItemDTO;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -10,49 +13,68 @@ import static java.util.stream.Collectors.toList;
 // TODO package private kontruktor och final attribute?
 public class Display {
     private final Sale sale;
+    private List<Item> listOfItems;
     Display(Sale sale) {
         this.sale = sale;
+        this.listOfItems = new ArrayList<>(sale.getCollectionOfItems());
     }
 
-    public String createEndOfSaleString() {
-        List<Item> listOfItems = new ArrayList<>(sale.getCollectionOfItems());
+    private List<SaleItemDTO> getSaleItemsInfo() {
+        List<SaleItemDTO> saleItemsInfo = new ArrayList<>();
+        for (Item item : listOfItems) {
+            saleItemsInfo.add(new SaleItemDTO(
+                    item.getItemDTO().getName(),        //itemName
+                    item.getItemDTO().getDescription(), //itemDescription
+                    item.getItemDTO().getPrice(),       //itemPrice
+                    item.getQuantity(),                 //itemQuantity
+                    item.getTotalAmount()               //totalPrice
+            ));
+        }
+        return saleItemsInfo;
+    }
+    public SaleDTO getSaleInfo() {
+        List<SaleItemDTO> saleItems = getSaleItemsInfo();
 
         // Momsberäkning
         Amount totalVATAmount = new Amount(0);
         List<Amount> vatAmounts = listOfItems.stream().map(Item::getVatAmount).collect(toList());
         totalVATAmount = totalVATAmount.plus(vatAmounts);
 
+        return new SaleDTO(
+                saleItems,              // list of saleItemInfo (DTO)
+                sale.getRunningTotal(), // Running total
+                totalVATAmount);        // VAT for the total sale
+    }
+    public String createRunningSaleString() {
+        // Sorterar listan efter när den reggats.
+        Collections.sort(listOfItems, Comparator.comparing(Item::getTimeOfUpdate).reversed());
+
+        // Pretty printing
+        StringBuilder builder = new StringBuilder();
+        SaleDTO currentSaleInfo = getSaleInfo();
+        builder.append(currentSaleInfo);
+        builder.append("\n");
+        builder.append("%-40s%s%n".formatted("Running total:", currentSaleInfo.getTotalPrice()));
+        return builder.toString();
+    }
+
+    public String createEndOfSaleString() {
         // Sorterar listan per namn
         Collections.sort(listOfItems, Comparator.comparing(Item::getName));
 
         // Pretty printing
         StringBuilder builder = new StringBuilder();
-        for (Item item: listOfItems) {
-            builder.append("%-40s%s%n".formatted(item.getItemDTO().getName(), item.getTotalAmount()));
-            builder.append("(" + item.getQuantity() + " * " + item.getItemDTO().getPrice() + "/each)\n");
-        }
+        SaleDTO saleInfo = getSaleInfo();
+        builder.append(saleInfo);
         builder.append("\n");
-        builder.append("%-40s%s%n".formatted("Running total:", this.sale.getRunningTotal()));
-        builder.append("%-40s%s%n".formatted("Total VAT:", totalVATAmount));
+        builder.append("%-40s%s%n".formatted("Total Price:", saleInfo.getTotalPrice()));
+        builder.append("%-40s%s%n".formatted("Total VAT:", saleInfo.getTotalVATAmount()));
         return builder.toString();
     }
 
     // TODO Eventuellt ändra till public String createRunningSaleString()
     @Override
     public String toString() {
-        List<Item> listOfItems = new ArrayList<>(sale.getCollectionOfItems());
-
-        // Sorterar listan efter när den registrerats
-        Collections.sort(listOfItems, Comparator.comparing(Item::getTimeOfUpdate).reversed());
-
-        // Pretty printing
-        StringBuilder builder = new StringBuilder();
-        for (Item item: listOfItems) {
-            builder.append("%-40s%s%n".formatted(item.getItemDTO().getName(), item.getTotalAmount()));
-            builder.append("(" + item.getQuantity() + " * " + item.getItemDTO().getPrice() + "/each)\n");
-        }
-        builder.append("\n");
-        builder.append("%-40s%s%n".formatted("Running total:", this.sale.getRunningTotal()));
-        return builder.toString();
+        return createRunningSaleString();
     }
 }
