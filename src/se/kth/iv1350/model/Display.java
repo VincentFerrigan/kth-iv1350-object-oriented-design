5d0f7ff862803de3553be20886bfb7bb7b192b42
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-04-28 10:57:22 +0200

    Adding the checkout shopping cart (end of sale) pretty printing. Adding proper indentation to the pretty printing.

--- src/se/kth/iv1350/model/Display.java
+++ src/se/kth/iv1350/model/Display.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 // TODO package private kontruktor och final attribute?
 public class Display {
     private final Sale sale;
@@ -12,6 +14,30 @@ public class Display {
         this.sale = sale;
     }
 
+    public String createEndOfSaleString() {
+        List<Item> listOfItems = new ArrayList<>(sale.getCollectionOfItems());
+
+        // Momsberäkning
+        Amount totalVATAmount = new Amount(0);
+        List<Amount> vatAmounts = listOfItems.stream().map(Item::getVatAmount).collect(toList());
+        totalVATAmount = totalVATAmount.plus(vatAmounts);
+
+        // Sorterar listan per namn
+        Collections.sort(listOfItems, Comparator.comparing(Item::getName));
+
+        // Pretty printing
+        StringBuilder builder = new StringBuilder();
+        for (Item item: listOfItems) {
+            builder.append("%-40s%s%n".formatted(item.getItemDTO().getName(), item.getTotalAmount()));
+            builder.append("(" + item.getQuantity() + " * " + item.getItemDTO().getPrice() + "/each)\n");
+        }
+        builder.append("\n");
+        builder.append("%-40s%s%n".formatted("Running total:", this.sale.getRunningTotal()));
+        builder.append("%-40s%s%n".formatted("Total VAT:", totalVATAmount));
+        return builder.toString();
+    }
+
+    // TODO Eventuellt ändra till public String createRunningSaleString()
     @Override
     public String toString() {
         List<Item> listOfItems = new ArrayList<>(sale.getCollectionOfItems());
@@ -22,12 +48,11 @@ public class Display {
         // Pretty printing
         StringBuilder builder = new StringBuilder();
         for (Item item: listOfItems) {
-            builder.append(item.getItemDTO().getName() + ", "); //TODO är det namn eller description
-            builder.append(item.getTotalAmount());
-            builder.append("\n");
+            builder.append("%-40s%s%n".formatted(item.getItemDTO().getName(), item.getTotalAmount()));
+            builder.append("(" + item.getQuantity() + " * " + item.getItemDTO().getPrice() + "/each)\n");
         }
         builder.append("\n");
-        builder.append("Running total: " + this.sale.getRunningTotal());
+        builder.append("%-40s%s%n".formatted("Running total:", this.sale.getRunningTotal()));
         return builder.toString();
     }
 }
