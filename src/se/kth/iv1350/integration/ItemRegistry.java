74596d574c818c1da29754d01593a0297f8f9c68
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-05-01 19:43:49 +0200

    ItemRegistry can now update the flat file db.

--- src/se/kth/iv1350/integration/ItemRegistry.java
+++ src/se/kth/iv1350/integration/ItemRegistry.java
@@ -5,31 +5,34 @@ import src.se.kth.iv1350.model.Amount;
 import src.se.kth.iv1350.model.Item;
 import src.se.kth.iv1350.model.VAT;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemRegistry {
-    private final String flat_file_db;
+    private final String flatFileDb;
+    private final String filePath;
     private Map<Integer, ItemData> inventoryTable = new HashMap<>();
 
     /**
      *
-     * @param file filename with relative path
+     * @param filePath relative file path
+     * @param fileName filename
      */
-    ItemRegistry(String file) {
+    ItemRegistry(String filePath, String fileName) {
         // TODO ska vi flytta ut hela inläsningsprocessen till en separat metod????
-        this.flat_file_db = file;
+        this.filePath = filePath;
+        this.flatFileDb = fileName;
         addItemData();
     }
     private void addItemData() {
         String splitCsvBy = ";";
+        FileReader reader;
         try{
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(this.flat_file_db));
+            reader = new FileReader(this.filePath + this.flatFileDb);
+            BufferedReader bufferedReader = new BufferedReader(reader);
             String line = "";
             line = bufferedReader.readLine();
             while((line = bufferedReader.readLine()) != null){
@@ -38,7 +41,7 @@ public class ItemRegistry {
                         Integer.parseInt(splitArray[0]),    //itemID
                         splitArray[1],                      //name
                         splitArray[2],                      //description
-                        Integer.parseInt(splitArray[3]),    //price
+                        Double.parseDouble(splitArray[3]),    //price
                         Integer.parseInt(splitArray[4]),   //vatRateGroupCode
                         Integer.parseInt(splitArray[5]),   //quantity
                         Integer.parseInt(splitArray[6]));   //quantity
@@ -52,7 +55,7 @@ public class ItemRegistry {
             System.out.println("IOE exception");
             e.printStackTrace();
         }
-       }
+    }
 
     /**
      *
@@ -71,7 +74,34 @@ public class ItemRegistry {
             inventoryTable.get(key).sold = (item.getQuantity()); // TODO alt. ta bort och ha enbart inStore?
             inventoryTable.get(key).inStore -= (item.getQuantity());
         }
-        // TODO Store in Flat based database
+        updateDatabase();
+    }
+
+    private void updateDatabase() {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(this.filePath + "inventory_" + LocalDate.now() + ".csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//            bufferedWriter.write("test;test;");
+            for (ItemData item : inventoryTable.values()) {
+//                bufferedWriter.write(String.valueOf(item));
+                bufferedWriter.write(item.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (FileNotFoundException e){
+            System.out.println("The file was not found");
+            e.printStackTrace(); //Skriver ut vart felet var någonstans.
+
+        } catch (IOException e){
+            System.out.println("IOE exception");
+            e.printStackTrace();
+//        } finally {
+//            try {
+//                buffered
+//            }
+        }
     }
 
     private static class ItemData {
@@ -96,7 +126,7 @@ public class ItemRegistry {
          * @param inStore           The quantity of items in stock
          * @param sold              The amount of items sold
          */
-        public ItemData(int articleNo, String name, String description, int price, int vatRateGroupCode, int inStore, int sold) {
+        public ItemData(int articleNo, String name, String description, double price, int vatRateGroupCode, int inStore, int sold) {
             this.articleNo = articleNo;
             this.name = name;
             this.description = description;
@@ -104,6 +134,22 @@ public class ItemRegistry {
             this.vat = new VAT(vatRateGroupCode);
             this.inStore = inStore;
             this.sold = sold;
+        }
+
+        @Override
+        public String toString() {
+            String splitCsvBy = ";";
+//            StringBuilder builder = new StringBuilder();
+//            builder.append(articleNo);
+//            builder.append(splitCsvBy);
+
+            return articleNo + splitCsvBy +
+                    name + splitCsvBy +
+                    description + splitCsvBy +
+                    price.getAmount() + splitCsvBy +
+                    vat.getVATRateGroupCode() + splitCsvBy +
+                    inStore + splitCsvBy +
+                    sold;
         }
     }
 }
