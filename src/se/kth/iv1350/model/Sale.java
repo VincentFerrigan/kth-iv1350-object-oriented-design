c0ea1a8c9e8384f4db866af88c9be9fa10dd44a5
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-05-01 19:46:47 +0200

    Sale can now add items to cart with only item id. Not used yet. To be discussed. this.items is renamed to shoppingCart. Discount functionality added.

--- src/se/kth/iv1350/model/Sale.java
+++ src/se/kth/iv1350/model/Sale.java
@@ -8,14 +8,18 @@ import src.se.kth.iv1350.integration.Display;
 import src.se.kth.iv1350.integration.ItemRegistry;
 import src.se.kth.iv1350.integration.Printer;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * Represent a particular sale.
  */
 public class Sale {
+    // ska timeOfSale vara final?
     private LocalDateTime timeOfSale;
-    private Amount runningTotal;
-    private Map<Integer, Item> items = new HashMap<>(); // TODO Ändra namn till shoppingCart?
+    private Map<Integer, Item> shoppingCart = new HashMap<>(); // TODO Ändra namn till shoppingCart?
     private CashPayment payment;
+
+    private DiscountDTO discount = new DiscountDTO();
 
     // TODO ska ett discount attribute finnas med i både sale och saleDTO?
     // TODO Ska den vara en tabell av rabatter, procentssats, belopp eller själva discountDTO?
@@ -28,24 +32,36 @@ public class Sale {
      */
     public Sale(ItemRegistry itemRegistry){
         this.timeOfSale = LocalDateTime.now();
-        this.runningTotal = new Amount(0);
+        this.itemRegistry = itemRegistry;
     }
 
-    // TODO varför inte Item istället för ItemDTO?
+    // TODO varför inte Item eller itemID istället för ItemDTO?
+    public void addItem(int itemID, int quantity) {
+        if (shoppingCart.containsKey(itemID)) {
+            this.shoppingCart.get(itemID).addToQuantity(quantity);
+        }
+        else {
+            ItemDTO itemInfo = itemRegistry.getItemInfo(itemID);
+            Item item = new Item(itemInfo, quantity);
+            shoppingCart.put(itemID, item);
+        }
+    }
+    public void addItem(int itemID) {
+        addItem(itemID, 1);
+    }
+
     public void addItem(ItemDTO itemInfo){
         addItem(itemInfo, 1);
-
     }
     public void addItem(ItemDTO itemInfo, int quantity){
-        Item additionalItem = new Item(itemInfo, quantity);
+        Item item = new Item(itemInfo, quantity);
 
         int key = itemInfo.getItemID(); // TODO hämta nyckeln från itemInfo eller additionalItem?
-        if (items.containsKey(key)){
-            this.items.get(key).addItem(additionalItem);
+        if (shoppingCart.containsKey(key)){
+            this.shoppingCart.get(key).addItem(item);
         } else {
-            items.put(key, additionalItem);
+            shoppingCart.put(key, item);
         }
-        this.runningTotal = this.runningTotal.plus(additionalItem.getTotalPrice());
     }
 
     private void increaseQuantity(){
@@ -60,11 +76,30 @@ public class Sale {
         return payment;
     }
     public Amount getRunningTotal() {
+        // Totalbelopp
+        Amount runningTotal = new Amount(0);
+        List<Amount> totalPrices = getCollectionOfItems()
+                .stream()
+                .map(Item::getTotalPrice)
+                .collect(toList());
+        runningTotal = runningTotal.plus(totalPrices);
+        runningTotal = runningTotal.multiply(discount.getDiscountMultiplier());
+
         return runningTotal;
     }
 
+    public Amount getTotalVATAmount() {
+        // Momsberäkning
+        Amount totalVATAmount = new Amount(0);
+        List<Amount> vatAmounts = getCollectionOfItems().stream().map(Item::getVatAmount).collect(toList());
+        totalVATAmount = totalVATAmount.plus(vatAmounts);
+        totalVATAmount = totalVATAmount.multiply(discount.getDiscountMultiplier());
+
+        return totalVATAmount;
+    }
+
     Collection<Item> getCollectionOfItems() {
-        return items.values();
+        return shoppingCart.values();
     }
 
     // TODO Bör nog ändras. Samma upplägg som Display. Logging kan ske med hjälp av SaleLog.
@@ -79,7 +114,7 @@ public class Sale {
     }
 
     public void applyDiscount(DiscountDTO discount){
-            //TODO also do it
+        this.discount = discount;
     }
 
     public void pay(CashPayment payment) {
@@ -91,13 +126,21 @@ public class Sale {
         printer.printReceipt(receipt);
     }
 
+//    public SaleDTO displayOpenSale(Display display) {
     public void displayOpenSale(Display display) {
         SaleOutput saleOutput = new SaleOutput(this);
         display.displayOpenSale(saleOutput);
+//        return saleOutput.getSaleInfo();
     }
 
+//    public SaleDTO displayCheckout(Display display) {
     public void displayCheckout(Display display) {
         SaleOutput saleOutput = new SaleOutput(this);
         display.displayCheckout(saleOutput);
+//        return saleOutput.getSaleInfo();
+    }
+
+    public void updateInventory() {
+        itemRegistry.updateInventory(getCollectionOfItems());
     }
 }
