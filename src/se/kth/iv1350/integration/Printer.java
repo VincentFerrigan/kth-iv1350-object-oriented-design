ef97055eaa6c8a37ebcb6087bc7b964081566a4a
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-05-01 19:44:24 +0200

    Printer only prints receipts. Adding a skeleton method for sale log.

--- src/se/kth/iv1350/integration/Printer.java
+++ src/se/kth/iv1350/integration/Printer.java
@@ -31,29 +31,4 @@ public class Printer {
         // TODO
     }
 
-    /**
-     * Displays the shopping cart with running total
-     * It's a dummy implementation that prints to
-     * <code>System.out</code>
-     * @param saleOutput The display
-     */
-    public void displayOpenSale(SaleOutput saleOutput) {
-        // TODO Eventuellt ändra till public String createRunningSaleString()
-        System.out.println("----------------- Display follows ----------------");
-        System.out.println(saleOutput.createOpenSaleString());
-        System.out.println("------------------ End of Display ----------------");
-
-    }
-
-    /**
-     * Displays the checkout shopping cart with total amount and vat.
-     * It's a dummy implementation that prints to
-     * <code>System.out</code>
-     * @param saleOutput The display
-     */
-    public void displayCheckout(SaleOutput saleOutput) {
-        System.out.println("--------------- End of Sale follows --------------");
-        System.out.println(saleOutput.createCheckoutString());
-        System.out.println("---------------- End of End of Sale --------------");
-    }
 }
