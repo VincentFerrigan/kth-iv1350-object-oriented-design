1964b606cf111c57ea536861292bf15e260e8dec
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-05-03 15:39:23 +0200

    Removing getTimeOfSale and getSaleItemsInfo.

--- src/main/se/kth/iv1350/dto/SaleDTO.java
+++ src/main/se/kth/iv1350/dto/SaleDTO.java
@@ -1,34 +1,22 @@
 package se.kth.iv1350.dto;
-import java.time.LocalDateTime;
 import java.util.List;
 import se.kth.iv1350.model.Amount;
 
 
 // TODO ska discountDTO finnas med i SaleDTO och/eller SaleItemDTO?
 public class SaleDTO {
-    private final LocalDateTime timeOfSale;
     private final List<SaleItemDTO> saleItemsInfo;
     private final Amount totalPrice;
     private final Amount totalVATAmount;
     public SaleDTO(List<SaleItemDTO> saleItemsInfo, Amount totalPrice, Amount totalVATAmount) {
-        this.timeOfSale = LocalDateTime.now();
         this.saleItemsInfo = saleItemsInfo;
         this.totalPrice = totalPrice;
         this.totalVATAmount = totalVATAmount;
     }
     public SaleDTO(SaleDTO other) {
-        this.timeOfSale = other.timeOfSale;
         this.saleItemsInfo = other.saleItemsInfo;
         this.totalPrice = other.totalPrice;
         this.totalVATAmount = other.totalVATAmount;
-    }
-
-    public LocalDateTime getTimeOfSale() {
-        return timeOfSale;
-    }
-
-    public List<SaleItemDTO> getSaleItemsInfo() {
-        return saleItemsInfo;
     }
 
     public Amount getTotalPrice() {
@@ -45,10 +33,8 @@ public class SaleDTO {
         StringBuilder builder = new StringBuilder();
         for (SaleItemDTO itemInfo: saleItemsInfo) {
             builder.append("%-40s%s%n".formatted(itemInfo.getName(), itemInfo.getTotalPrice()));
-            builder.append("(" + itemInfo.getQuantity() + " * " + itemInfo.getUnitPrice() + "/st.)\n");
+            builder.append("(%d * %s)\n".formatted(itemInfo.getQuantity(), itemInfo.getUnitPrice()));
         }
-//        builder.append("\n");
-//        builder.append("%-40s%s%n".formatted("Running total:",totalPrice));
         return builder.toString();
     }
 }
