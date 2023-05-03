ae9ee218a6c9fe9cfa5fcc080104d828d8e9bf9f
Elin Blomquist <eblomquisst@gmail.com>
2023-05-03 12:40:49 +0200

    Start of setup

--- src/test/se/kth/iv1350/model/SaleTest.java
+++ src/test/se/kth/iv1350/model/SaleTest.java
@@ -4,16 +4,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaleTest {
+    private Map<Integer, Item> testCart;
+    private final int testItemID = 4;
 
     @BeforeEach
     void setUp() {
+        testCart = new HashMap<>();
     }
 
     @AfterEach
     void tearDown() {
+        testCart = null;
     }
 
     @Test
