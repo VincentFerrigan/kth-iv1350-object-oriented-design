e116362f92d62380195cc8b7f03354c1917fe386
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-05-11 13:31:04 +0200

    Code/Data harmonization

--- src/main/se/kth/iv1350/util/LogHandler.java
+++ src/main/se/kth/iv1350/util/LogHandler.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Locale;
 
 /**
  * This class is responsible for the log.
@@ -13,6 +14,8 @@ import java.time.format.FormatStyle;
 public class LogHandler {
     private static final String FILE_PATH = "src/main/se/kth/iv1350/data/";
     private static final String LOG_FILE_NAME = "pos-log.txt";
+    private Locale locale = new Locale("sv", "SE");
+    private DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).localizedBy(locale);
     private PrintWriter logFile;
 
     public LogHandler() throws IOException {
@@ -36,7 +39,6 @@ public class LogHandler {
 
     private String createTime() {
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
         return now.format(formatter);
     }
 }
