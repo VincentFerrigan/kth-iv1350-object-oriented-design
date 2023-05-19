93f1086713161b15f1e12359dbb7fa433da261a4
Vincent Ferrigan <70985805+VincentFerrigan@users.noreply.github.com>
2023-05-19 07:25:12 +0200

    Singleton (#6)
    * LogHandler converted to a singleton. Also adding a Logger interface.
    
    * Both LogHandler and ErrorMessageHandler are remade to Singletons. They both implement the logger interface. LogHandler is renamed to ErrorFileLogHandler for clarity.
    
    * Adding important TODOs in RegisterCreater. In short, Open Issue: Make all registers into Singletons. Should I also use the Template Model Design?
    
    * Making AS to a Singleton. However, not sure if I should add nested class and map inorder to abstract all the Dbs
    
    * Making the "external systems" into Singletons. A lot of semi-repetitive code. Can I abstract these?
    
    * Making filepaths OS agnostic.
    
    * Code restructure/cleaning
    
    * Adding two exceptions. One for item registry and one for shopping cart
    
    * Typeo
    
    * Making file path os agnostic.
    
    * Renamed the exception item not found
    
    * Moving item registry away from sale. Product catalog in control instead.
    
    * Refactoring (renamed some classes and methods). Also adding todos
    
    * Removing DTO returns to view. Also temp. removing discount. Combining a strategy and composite pattern for discount.
    
    * Making timeOfUpdate final. All DTO attributes should be final.
    
    * Temp. removing discounts form accounting system.
    
    * Updating CD

--- src/main/se/kth/iv1350/view/SaleView.java
+++ src/main/se/kth/iv1350/view/SaleView.java
@@ -5,6 +5,7 @@ import se.kth.iv1350.model.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO UML:a och skriva JavaDocs
 /**
  * A template for a sale observer that shows sale details including the shopping cart.
  */
