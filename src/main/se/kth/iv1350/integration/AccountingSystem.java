3c71b4ac68ef8fb427efa4abc48a5dcc5d3cc004
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-06-01 16:53:16 +0200

    javadoc

--- src/main/se/kth/iv1350/integration/AccountingSystem.java
+++ src/main/se/kth/iv1350/integration/AccountingSystem.java
@@ -3,5 +3,11 @@ package se.kth.iv1350.integration;
 import se.kth.iv1350.model.Sale;
 
 public interface AccountingSystem {
+
+    /**
+     * Updates the accounting system.
+     * @param closedSale contains the sale details
+     * @throws AccountingSystemException when database call failed.
+     */
     void updateRegistry(Sale closedSale);
 }
