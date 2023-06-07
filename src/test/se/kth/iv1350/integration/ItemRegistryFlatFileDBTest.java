2812f9026bb6e98df5d8813b69754353e1c45a02
Vincent Ferrigan <vincent.ferrigan@gmail.com>
2023-06-07 16:02:40 +0200

    Additional tests (#49)
    * Corrected the month assertion test
    
    * The testUpdateRegistry is not working properly yet.
    
    * Renamed test method name, to follow naming convention.

--- src/test/se/kth/iv1350/integration/ItemRegistryFlatFileDBTest.java
+++ src/test/se/kth/iv1350/integration/ItemRegistryFlatFileDBTest.java
@@ -20,7 +20,10 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
     ItemRegistryFlatFileDB instance;
     private final String FILE_PATH_KEY = "se.kth.iv1350.database.file.location";
     private final String FLAT_FILE_DB_NAME_KEY = "se.kth.iv1350.database.file.inventory_db";
+    String fileName = "item-inventory-test.csv";
     private File flatFileDb;
+    String recordHeader = "ITEM ID;NAME;DESCRIPTION;PRICE;VAT RATE GROUP CODE;IN STORE;SOLD";
+    String itemRecord = "0;test name;test description;8;1;10;0";
 
     @BeforeEach
     void setUp() {
@@ -28,23 +31,32 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
         flatFileDb = new File(
                 System.getProperty(FILE_PATH_KEY) +
                         System.getProperty("file.separator") +
-                        System.getProperty(FLAT_FILE_DB_NAME_KEY));
+                        fileName);
+
+        try {
+            flatFileDb.createNewFile();
+        } catch (IOException ex) {
+            fail("No exception should be thrown, unable to to create a new flat db file."
+                    + "%s".formatted(ex.getMessage()));
+        }
     }
 
     @AfterEach
     void tearDown() {
         instance = null;
+//        flatFileDb.delete();
         flatFileDb = null;
+
     }
 
     @Test
-    void getInstance() {
+    void testGetInstance() {
         ItemRegistryFlatFileDB newInstance = ItemRegistryFlatFileDB.getInstance();
         assertEquals(instance, newInstance);
     }
 
     @Test
-    void getDataInfo() {
+    void testGetDataInfo() {
         ItemDTO itemInfo = null;
         try {
             itemInfo = instance.getDataInfo(0);
@@ -57,7 +69,22 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
 
     @Disabled
     @Test
-    void updateRegistry() {
+    void testUpdateRegistry() {
+        System.setProperty(FLAT_FILE_DB_NAME_KEY, fileName);
+        try (FileWriter fileWriter = new FileWriter(flatFileDb);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(recordHeader);
+            bufferedWriter.newLine();
+            bufferedWriter.write(itemRecord);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (FileNotFoundException ex){
+            fail("No exception should be thrown, unable to set up the registry test. "
+                    + "%s".formatted(ex.getMessage()));
+        } catch (IOException ex){
+            fail("No exception should be thrown, unable to set up the registry test. "
+                    + "%s".formatted(ex.getMessage()));
+        }
         String firstRecordStringPreUpdate = "";
         String firstRecordStringPostUpdate = "";
         String expFirstRecordStringPostUpdate = "";
@@ -77,12 +104,8 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
         Sale sale = new Sale();
         try {
             sale = new Sale();
-        } catch (PricingFailedException ex) {
-            // Not part of the test
-            fail("No exception should be thrown, unable to set up the SaleLogTest. "
-                    + "%s".formatted(ex.getMessage()));
-        }
-        try {
+            sale.addItem(0, 1);
+            sale.addItem(0, 1);
             sale.addItem(0, 1);
         } catch (ItemNotFoundInItemRegistryException | PricingFailedException | ItemRegistryException e) {
             fail("Exception should not have been thrown, " +
@@ -91,8 +114,9 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
         sale.endSale();
         sale.pay(new CashPayment(new Amount(100)));
         instance.updateRegistry(sale);
+        RegistryHandler.getInstance().updateItemRegistry(sale);
 
-        try (FileReader reader = new FileReader(flatFileDb.getPath().replace(".csv", "-" + LocalDate.now()));
+        try (FileReader reader = new FileReader(flatFileDb);
              BufferedReader bufferedReader = new BufferedReader(reader)) {
             bufferedReader.readLine();
             firstRecordStringPostUpdate = bufferedReader.readLine();
@@ -104,6 +128,9 @@ class ItemRegistryFlatFileDBTest extends POSTestSuperClass {
                     + "%s".formatted(ex.getMessage()));
         }
 
-            assertFalse(firstRecordStringPostUpdate.equals(firstRecordStringPreUpdate));
+//        assertFalse(firstRecordStringPostUpdate.equals(firstRecordStringPreUpdate));
+        System.out.println(firstRecordStringPreUpdate);
+        System.out.println(firstRecordStringPostUpdate);
+
     }
 }
