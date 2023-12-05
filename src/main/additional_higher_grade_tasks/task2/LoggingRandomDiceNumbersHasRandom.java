aa89f307c7e00d6dbb822aa5d294151674ba681a
VincentFerrigan <vincent.ferrigan@gmail.com>
2023-12-05 13:33:56 +0100

    Adding additional-point assignments

---
+++ src/main/additional_higher_grade_tasks/task2/LoggingRandomDiceNumbersHasRandom.java
@@ -0,0 +1,13 @@
+package additional_higher_grade_tasks.task2;
+
+import java.util.Random;
+
+public class LoggingRandomDiceNumbersHasRandom {
+    private Random random = new Random();
+
+    public int nextInt() {
+        int result = random.nextInt(6) +1;
+        System.out.println("Generated random dice number: " + result);
+        return result;
+    }
+}
